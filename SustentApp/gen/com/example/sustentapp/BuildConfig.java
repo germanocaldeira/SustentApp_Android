bba39963be2e62c121277740fb5c2720c1e7a8de
Germano Caldeira <germanocaldeira@gmail.com>
2014-06-09 22:54:15 -0300

    Revert "First commit"
    This reverts commit d8d04c2aa6bce8abbba75cc17338169ac54fea26.

--- SustentApp/gen/com/example/sustentapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.sustentapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- SustentApp/src/com/example/sustentapp/CadastroUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.example.sustentapp;
-
-import com.example.sustentapp.Usuario;
-
-public class CadastroUsuario {
-	
-	
-
-}
